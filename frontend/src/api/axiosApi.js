8aa3515f0e5a6f4719868777e41132f63d12b13b
sdrtba <sdrtba@yandex.ru>
2025-04-29 22:50:35 +0400

    fixes

--- frontend/src/api/axiosApi.js
+++ frontend/src/api/axiosApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'http://213.108.23.238/api',
   headers: {
     'Content-Type': "application/json",
   },
